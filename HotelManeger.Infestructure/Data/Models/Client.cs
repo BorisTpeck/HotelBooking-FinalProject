60eed3e3494b85b9cd28a8b4e675a129041abccd
BorisTpeck <boris.tsonchev.highschool@buditel.bg>
2025-04-30 13:16:19 +0300

    Push

---
+++ HotelManeger.Infestructure/Data/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelManeger.Infestructure.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string FamilyName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PersonalId { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public ICollection<Stay> Stays { get; set; } = new List<Stay>();
+    }
+}
